2ecff3d187258e358497b2ffcde480590514c2a7
yukihira09 <84973224+yukihira09@users.noreply.github.com>
2023-10-24 01:58:57 +0700

    Remove unused libraries

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import matplotlib.pyplot as plt
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
 from kivy.uix.camera import Camera
-from kivy.uix.popup import Popup
 from kivy.core.window import Window
 Window.size = (430, 932)
 import torch
@@ -13,13 +11,9 @@ import cv2
 import numpy as np
 from PIL import Image as PILImage
 from kivy.uix.image import Image
-import torchvision.transforms as transforms
 from model import DetectNet
 from ultralytics import YOLO
-
 from kivy.uix.label import Label
-from kivymd.theming import ThemeManager
-from kivy.uix.modalview import ModalView
 
 import os
 
