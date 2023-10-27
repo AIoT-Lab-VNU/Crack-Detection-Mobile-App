70c9608c1313fa812c7ffc1f60cdde275dee3186
dangkhoi <khoindd2000@gmail.com>
2023-10-27 10:43:17 +0700

    deploy

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
-from kivy.lang import Builder
+# from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
 from kivy.uix.camera import Camera
-from kivy.uix.popup import Popup
+# from kivy.uix.popup import Popup
 from kivy.core.window import Window
 Window.size = (430, 932)
-import torch
+# import torch
 import cv2
 import numpy as np
 from PIL import Image as PILImage
-from kivy.uix.image import Image
-import torchvision.transforms as transforms
+# from kivy.uix.image import Image
+# import torchvision.transforms as transforms
 from model import DetectNet
 from ultralytics import YOLO
-import os
-from kivymd.toast import toast
+# import os
+# from kivymd.toast import toast
 
 # Loading pretrained model
 yolo = YOLO("weights/best.pt")
