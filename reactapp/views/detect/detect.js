80b97eb0fc9f8a4429602a3ed077848c75fdecb8
nddkhoi <nddkhoi@ai4ia.cc>
2023-11-15 00:24:25 +0700

    done capture camer

--- reactapp/views/detect/detect.js
+++ reactapp/views/detect/detect.js
@@ -1,41 +1,45 @@
 import React from 'react';
+import { useRef } from 'react';
 import { Text, View, Image, TouchableOpacity } from 'react-native';
 import styles from './detectStyles.js';
 import Header from '../../components/header/header.js';
 import { RNCamera } from 'react-native-camera';
+import { Icon } from 'react-native-elements';
+
 
 export default function Detect() {
-    
+    const cameraRef = useRef(null);
     const takePicture = async () => {
-        if (this.camera) {
-          const options = { quality: 0.5, base64: true };
-          const data = await this.camera.takePictureAsync(options);
-          console.log(data.uri);
+        if(cameraRef.current){
+            const options = { quality: 0.5, base64: true };
+            const data = await cameraRef.current.takePictureAsync(options);
+
+            // Display the captured image
+            console.log('Captured Image:', data.base64);
         }
-      };
+    }
 
     return (
         <View style={styles.container}>
             <Header isChild={true} />
             <RNCamera
-                ref={ref => {
-                    this.camera = ref;
-                }}
                 style={styles.cameraContainer}
-                type={RNCamera.Constants.Type.back}
-                flashMode={RNCamera.Constants.FlashMode.on}
-                androidCameraPermissionOptions={{
-                    title: 'Permission to use camera',
-                    message: 'We need your permission to use your camera',
-                    buttonPositive: 'Ok',
-                    buttonNegative: 'Cancel',
-                }}
-            />
-            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-                <TouchableOpacity onPress={() => takePicture(this)} style={styles.capture}>
-                    <Text style={{ fontSize: 14 }}> SNAP </Text>
-                </TouchableOpacity>
-            </View>
+                ref={cameraRef}
+            >
+                <View style={styles.btnContainer}>
+                    <TouchableOpacity style={styles.btnLib}>
+                        <Icon
+                            type="ionicon"
+                            name="images-outline"
+                            color="#FFFFFF"
+                            iconProps={{ size: 50 }}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btnCapture} onPress={takePicture}>
+                        <Text style={{ fontSize: 14 }}>SNAP</Text>
+                    </TouchableOpacity>
+                </View>
+            </RNCamera>
         </View>
     )
 }
--- reactapp/views/detect/detectStyles.js
+++ reactapp/views/detect/detectStyles.js
@@ -1,13 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('screen').width;
+const screenHeight = Dimensions.get('screen').height;
 
 const styles = StyleSheet.create({
     container: {
-
+        height: '100%'
     },
     cameraContainer: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center',
+        // flex: 1,
+        // justifyContent: 'flex-end',
+        // alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'rgba(0, 0, 0, 0.5)', // Adjust the alpha value as needed
+        borderStyle: 'solid',
+        position: 'relative',
+        top: 0,
+        width: screenWidth,
+        height: screenHeight,
+        // display: 'flex',
+        // flexDirection: 'row',
+        // alignItems: 'flex-end'
     },
     capture: {
         flex: 0,
@@ -18,6 +31,35 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         margin: 20,
       },
+      btnContainer: {
+        position: 'absolute',
+        width: '100%',
+        height: 80,
+        bottom: 150,
+        // backgroundColor: 'rgba(0,0,0,0.5)',
+        display: 'flex',
+        alignItems: 'center'
+      },
+      btnCapture: {
+        width: 50,
+        position: 'absolute',
+        left: '50%',
+        top: '50%',
+        transform: [{translateY: -50}, {translateX: -50}],
+        width: 80,
+        height: 80,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        borderRadius: 80,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: '10px solid #fff'
+      },
+      btnLib: {
+        width: 50,
+        position: 'absolute',
+        left: 20
+      }
 })
 
 export default styles;
